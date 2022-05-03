2f270a2789270c9c1691a35ea4be86bfa1d84632
鈺翔 林 <lonely840314@outlook.com>
2022-05-03 16:54:12 +0800

    5-3

---
+++ 36_Linq/Frm作業_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHomeWork
+{
+    public partial class Frm作業_2 : Form
+    {
+        public Frm作業_2()
+        {
+            InitializeComponent();
+            this.productPhotoTableAdapter1.Fill(this.awDataSet11.ProductPhoto);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.DataSource = this.awDataSet11.ProductPhoto;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var p = from ph in this.awDataSet11.ProductPhoto
+                    where ph.ModifiedDate BETWEEN
+
+
+        }
+    }
+}
